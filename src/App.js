6f86178df42eafdc3ce11013c705eefd80140387
slyxh2 <750772121@qq.com>
2023-02-12 23:06:54 -0500

    sidebar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
     return <div>
         <BrowserRouter>
             <div className='flex relative dark:bg-main-dark-bg'>
